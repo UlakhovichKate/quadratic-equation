fc6af8053e37e0e50105e29e6d8d2b48916eec1e
Katsiaryna <36642433+UlakhovichKate@users.noreply.github.com>
2018-02-25 15:14:59 +0300

    result 154

--- src/index.js
+++ src/index.js
@@ -6,18 +6,34 @@ module.exports = function solveEquation(equation) {
   let c = array.slice(7,10).join('');
   var d = b * b - 4 * a * c;
   var Arr=[];
-  if ( d < 0 ) {
-    res1= - b / ( 2 * a ) + Math.sqrt( -d ) / ( 2 * a );
-    res2= - b / ( 2 * a) - Math.sqrt( -d ) / ( 2 * a );
-  }
-  if ( d == 0 ) {
-      res1= -b / ( 2 * a );
-      res2= -b / ( 2 * a );
-  }
-  else {
-        res1= -b / ( 2 * a ) - Math.sqrt( d ) / ( 2 * a );
-        res2= -b / ( 2 * a ) + Math.sqrt( d ) / ( 2 * a );
-      }
-  Arr.push(res1,res2);
+   if ( d < 0 ) {
+     res1=Math.ceil( - b / ( 2 * a ) + Math.sqrt( -d ) / ( 2 * a ));
+     res2=Math.ceil( - b / ( 2 * a) - Math.sqrt( -d ) / ( 2 * a ));
+     if (res1<res2){
+       r1=res1;
+       r2=res2;
+     }
+     else{
+       r1=res2;
+       r2=res1;
+     }
+   }
+   if ( d == 0 ) {
+       r1= -b / ( 2 * a );
+       r2= -b / ( 2 * a );
+   }
+   else {
+         res1=Math.ceil( -b / ( 2 * a ) - Math.sqrt( d ) / ( 2 * a ));
+         res2=Math.ceil( -b / ( 2 * a ) + Math.sqrt( d ) / ( 2 * a ));
+         if (res1<res2){
+           r1=res1;
+           r2=res2;
+         }
+         else{
+           r1=res2;
+           r2=res1;
+         }
+       }
+   Arr.push(r1,r2);
   return Arr;
 }
