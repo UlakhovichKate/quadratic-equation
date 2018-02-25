0444869825b3e735e12581b74f16b5fb9e460e62
Katsiaryna <36642433+UlakhovichKate@users.noreply.github.com>
2018-02-25 14:08:57 +0300

    74 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let array = equation.split(' ');
+  let a = array[0];
+  let b = array.slice(3,5).join('');
+  let c = array.slice(7,10).join('');
+  var d = b * b - 4 * a * c;
+  var Arr=[];
+  if ( d < 0 ) {
+    res1= - b / ( 2 * a ) + Math.sqrt( -d ) / ( 2 * a );
+    res2= - b / ( 2 * a) - Math.sqrt( -d ) / ( 2 * a );
+  }
+  if ( d == 0 ) {
+      res1= -b / ( 2 * a );
+      res2= -b / ( 2 * a );
+  }
+  else {
+        res1= -b / ( 2 * a ) - Math.sqrt( d ) / ( 2 * a );
+        res2= -b / ( 2 * a ) + Math.sqrt( d ) / ( 2 * a );
+      }
+  Arr.push(res1,res2);
+  return Arr;
 }
